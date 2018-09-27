527dddce65813734882bb099fdaf54efa94382dc
VadBezmenov <bezmenof@gmail.com>
2018-09-27 22:34:03 +0300

    Task2

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args);
+
+    int numb = 777;
+        if(numb %10);
+    {
+     System.out.println("last numb is 7");
+} else {
+        System.out.println("last numb is not a 7");}
+
+}
